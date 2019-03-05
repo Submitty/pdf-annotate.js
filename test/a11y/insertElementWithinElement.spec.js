386f98f7b67f237e2dac26f329d7187779841640
Matthew Peveler <matt.peveler@gmail.com>
2019-03-04 19:07:43 -0500

    Try and debug Chrome test failures on Travis

--- test/a11y/insertElementWithinElement.spec.js
+++ test/a11y/insertElementWithinElement.spec.js
@@ -1,6 +1,6 @@
 import insertElementWithinElement from '../../src/a11y/insertElementWithinElement';
 import mockPageWithTextLayer, { CHAR_WIDTH } from '../mockPageWithTextLayer';
-import { equal } from 'assert';
+import { strictEqual } from 'assert';
 
 function createElement(content) {
   let el = document.createElement('div');
@@ -27,13 +27,13 @@ describe('a11y::insertElementWithinElement', function () {
   it('should insert an element within another element', function () {
     let el = createElement();
     let result = insertElementWithinElement(el, rect.left + 10 + (CHAR_WIDTH * 5), rect.top + 15, 1);
-    equal(result, true);
+    strictEqual(result, true);
   });
   
   it('should not insert if no element can be found', function () {
     let el = createElement();
     let result = insertElementWithinElement(el, rect.left, rect.top + 25, 1);
-    equal(result, false);
+    strictEqual(result, false);
   });
 
   it('should insert an element at the proper point', function () {
@@ -41,7 +41,7 @@ describe('a11y::insertElementWithinElement', function () {
     let textLayer = page.querySelector('.textLayer');
     insertElementWithinElement(el, rect.left + 10 + (CHAR_WIDTH * (process.env.CI === 'true' ? 6 : 5)), rect.top + 15, 1);
     let node = textLayer.children[0];
-    equal(node.innerHTML, 'abcde<div>hello</div>fghijklmnopqrstuvwxyz');
+    strictEqual(node.innerHTML, 'abcde<div>hello</div>fghijklmnopqrstuvwxyz');
   });
 
   it('should not insert within a nested element', function () {
@@ -50,6 +50,6 @@ describe('a11y::insertElementWithinElement', function () {
     let node = textLayer.children[0];
     node.innerHTML = node.innerHTML.replace('ef', 'e<img>f');
     insertElementWithinElement(el, rect.left + 10 + (CHAR_WIDTH * (process.env.CI === 'true' ? 6 : 5)), rect.top + 15, 1);
-    equal(node.innerHTML, 'abcde<div>hello</div><img>fghijklmnopqrstuvwxyz');
+    strictEqual(node.innerHTML, 'abcde<div>hello</div><img>fghijklmnopqrstuvwxyz');
   });
 });
