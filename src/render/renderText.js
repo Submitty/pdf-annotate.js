6fe0ca8e769960e243c3e74f399447e151406935
Eddie Krystowski <eddiekrystowski@gmail.com>
2021-07-22 16:17:08 -0400

    linting

--- src/render/renderText.js
+++ src/render/renderText.js
@@ -4,21 +4,21 @@ import normalizeColor from '../utils/normalizeColor';
 /**
  * Wrap each line of given text in a `<tspan>` element and append these
  * lines to the given SVGTextElement
- * 
+ *
  * @param {SVGTextElement} textElement A text element to hold the split text
  * @param {String} textContent String to render with line breaks
  */
 function insertLineBreaks(textElement, textContent) {
   const lines = (textContent || '').split('\n');
-  //can't use dy attribute here since we want empty lines to take up space as well,
-  //so we will update y manually based on font size
+  // can't use dy attribute here since we want empty lines to take up space as well,
+  // so we will update y manually based on font size
   const x = textElement.getAttribute('x');
   let y = Number(textElement.getAttribute('y'));
   const size = Number(textElement.getAttribute('font-size'));
   for (const line of lines) {
     const tspan = document.createElementNS('http://www.w3.org/2000/svg', 'tspan');
     tspan.setAttribute('y', y.toString());
-    tspan.setAttribute('x', textElement.getAttribute('x'));
+    tspan.setAttribute('x', x);
     tspan.innerHTML = line;
     textElement.appendChild(tspan);
 
